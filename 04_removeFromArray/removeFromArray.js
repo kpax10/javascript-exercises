22ce8bc2787c9c1a7dde3199178eda4a222b5a8a
Kyle <notxap55@yahoo.com>
2022-04-27 15:53:58 -0700

    non functional yet

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, num) {
+    array.splice(arr.indexOf(num));
 };
-
+console.log(removeFromArray[1, 2, 3, 4], 3);
+// console.log(removeFromArray([1, 2, 3, 4], 3));
 // Do not edit below this line
-module.exports = removeFromArray;
+//module.exports = removeFromArray;
+
+// const array = [1, 2, 3];
+// array.pop();
+// console.log(array);
