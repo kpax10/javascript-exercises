c3591a0f114482fec91321cd859858dc30ad1757
Kyle <notxap55@yahoo.com>
2022-04-26 22:35:20 -0700

    exercise 3 started

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    // loop through string backwards, adding each letter to an empty reverse array.
 
+    // join the reverse array
 };
 
 // Do not edit below this line
