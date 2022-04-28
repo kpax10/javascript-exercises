e18c4dad39d08af7ba70e3e29060a4a1e9292bc7
Kyle <notxap55@yahoo.com>
2022-04-28 15:50:36 -0700

    test 1 and 2 passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
+const sumAll = function (getNum1, getNum2) {
+    const array = [];
+    let sum = 0;
 
+    // create array
+    for (let i = getNum1; i <= getNum2; i++) array.push(i);
+    // add array to sum
+    for (let elem of array) sum += elem;
+    return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
