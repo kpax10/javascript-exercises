2a6862f88d73ab494b22fccb93594451ae83a20a
Kyle <notxap55@yahoo.com>
2022-04-27 21:36:16 -0700

    continuing tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
 const removeFromArray = function (arr, ...theArgs) {
+    // filter the array??
+
     for (let i = 0; i < theArgs.length; i++) {
-        arr.splice(arr.indexOf(theArgs[i]), 1);
+        if (arr[i] === theArgs[i]) {
+            break;
+        } else arr.splice(arr.indexOf(theArgs[i]), 1);
     }
     return arr;
 }
@@ -8,3 +12,6 @@ console.log(removeFromArray([1, 2, 3, 4], 7, 2));
 
 // Do not edit below this line
 //module.exports = removeFromArray;
+// const arr = [4, 5, 6];
+// arr.splice(arr.indexOf(7))
+// console.log(arr);
