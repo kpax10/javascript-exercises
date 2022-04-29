ded163f76a78e4b12641dec5092d67b8e66c1d2d
Kyle <notxap55@yahoo.com>
2022-04-29 15:54:58 -0700

    started tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    if (year % 400 === 0 && year % 100 === 0) return false;
+    if (year % 400 === 0 && year % 100 === 0) return false;
+
+    // if % by 400  && 4 true
+    // if % by 100 && 4 false
+
 
 };
+console.log(leapYears(2000));
 
 // Do not edit below this line
 module.exports = leapYears;
