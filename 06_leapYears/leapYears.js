34ef22f43f42647c60f7050f7d38438340e4fa5c
Kyle <notxap55@yahoo.com>
2022-04-30 08:07:29 -0700

    refactored

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,6 @@
 const leapYears = function (year) {
-    if (year % 400 === 0) return true;
-    if ((year % 4 === 0) && (year % 100 !== 0)) {
-        return true;
-    } else return false;
+    return ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) ? true : false;
 };
-console.log(leapYears(2000));
 
 // Do not edit below this line
 module.exports = leapYears;
