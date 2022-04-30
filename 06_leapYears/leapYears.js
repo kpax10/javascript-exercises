9dce443a4c88cde6480e81d39fc4eb78a8511bf0
Kyle <notxap55@yahoo.com>
2022-04-29 17:50:31 -0700

    all tests complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,11 +1,8 @@
 const leapYears = function (year) {
-    if (year % 400 === 0 && year % 100 === 0) return false;
-    if (year % 400 === 0 && year % 100 === 0) return false;
-
-    // if % by 400  && 4 true
-    // if % by 100 && 4 false
-
-
+    if (year % 400 === 0) return true;
+    if ((year % 4 === 0) && (year % 100 !== 0)) {
+        return true;
+    } else return false;
 };
 console.log(leapYears(2000));
 
