377ffcf8e741a522d4e8fc6bf189a5271fe8e68e
Kyle <notxap55@yahoo.com>
2022-06-22 13:33:40 -0700

    all tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,29 +3,32 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 
 const sum = function (array) {
-  // let total = 0;
-  // for (let i = 0; i < array.length; i++) {
-  //   total += array[i];
-  //   // return total;
-  // } return total;
-
   let total = 0;
+
   array.forEach(element => {
     total += element;
   });
   return total;
+};
+
+const multiply = (array) => {
+  let total = 1;
+  array.forEach(element => {
+    total *= element;
+  });
+  return total;
 }
 
-const multiply = function () {
+const power = (a, b) => a ** b;
 
-};
+const factorial = (num) => {
+  if (num === 0) return 1;
 
-const power = function () {
-
-};
-
-const factorial = function () {
-
+  let total = 1;
+  for (let i = num; i > 0; i--) {
+    total *= i;
+  };
+  return total;
 };
 
 // Do not edit below this line
