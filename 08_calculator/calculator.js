584519a98541b022b4ffbe38b093eda6c15f5e89
Kyle <notxap55@yahoo.com>
2022-06-22 12:27:51 -0700

    add and subtract, sum started

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const sum = function (array) {
+  // let total = 0;
+  // for (let i = 0; i < array.length; i++) {
+  //   total += array[i];
+  //   // return total;
+  // } return total;
+
+  let total = 0;
+  array.forEach(element => {
+    total += element;
+  });
+  return total;
+}
+
+const multiply = function () {
+
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const power = function () {
 
 };
 
-const power = function() {
-	
-};
+const factorial = function () {
 
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
