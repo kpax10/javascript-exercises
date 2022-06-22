56276f1162323e75930733b76de463cdce64427d
Kyle <notxap55@yahoo.com>
2022-06-22 15:35:35 -0700

    all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const array = [];
 
+  for (let i = 0; i < string.length; i++) {
+    if (
+      (string[i] === ' ') ||
+      (string[i] === '.') ||
+      (string[i] === ',') ||
+      (string[i] === '!')
+    ) continue
+    array.push(string[i].toLowerCase());
+  };
+
+  let j = array.length - 1;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === array[j]) {
+      j--;
+    } else return false;
+  } return true
 };
-
 // Do not edit below this line
 module.exports = palindromes;
