48b237c8fbd9dfc2a867056c4fc3bcb9574b21ae
Kyle <notxap55@yahoo.com>
2022-06-22 21:49:58 -0700

    pseudo code started

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+
+  // loop x amount of times, adding numbers to array.  first two numbers are 1.
 
 };
+
+//fibonacci(4) // returns the 4th member of the series: 3  (1, 1, 2, 3)
 
 // Do not edit below this line
 module.exports = fibonacci;
