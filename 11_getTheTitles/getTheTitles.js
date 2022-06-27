c95f39e6f868731319e560f9aa97ef43e058b4ac
Kyle <notxap55@yahoo.com>
2022-06-27 11:12:36 -0700

    tests passed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
 
-};
+// const getTheTitles = function (array) {
+//   return array.map(el => el.title)
+// };
+
+const getTheTitles = (array) => array.map(el => el.title);
 
 // Do not edit below this line
 module.exports = getTheTitles;
