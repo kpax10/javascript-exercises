1d41d9fda1e55d41fc1a911c82b6f00cc91fc859
Kyle <notxap55@yahoo.com>
2022-06-27 15:12:36 -0700

    test 1 passed, needs refactoring

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
 
-};
+const findTheOldest = function (array) {
+
+  array.sort((a, b) => {
+
+    if ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
+      return -1;
+    } else return 1;
+  })
+
+  return array[0];
+
+}
+
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
